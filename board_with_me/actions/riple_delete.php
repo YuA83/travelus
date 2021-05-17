17652e54229a451f2c39afbaacd93a92f315aa5b
DESKTOP-8OM2URP\kga_5 <goom0803@naver.com>
2021-05-17 14:24:11 +0900

    upload01

---
+++ board_with_me/actions/riple_delete.php
@@ -0,0 +1,24 @@
+<?php
+    include '../core/db.php';
+    include '../core/write_session.php';
+    session_start();
+    
+    $num = $_REQUEST['num'];
+    $sql = "SELECT * FROM riple2 WHERE num='$num'";
+    $res = mysqli_query($conn,$sql) or die(mysqli_error($conn));
+    $rows = mysqli_fetch_array($res);
+    $delete = "DELETE FROM riple2 WHERE num = '$num'";
+    if($rows['id'] !=$_SESSION['username']){
+    ?>
+        <script>
+            alert("권한이 없습니다.");
+            history.back();
+        </script>
+<?php   } else {
+        mysqli_query($conn,$delete);
+?>      <script>
+            alert("삭제되었습니다.");
+            history.back();
+        </script>    
+<?php   }
+?>
