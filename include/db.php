d2ac550294a777fe385c4acf99105d7e10afd148
DESKTOP-8OM2URP\kga_5 <goom0803@naver.com>
2021-05-17 15:17:12 +0900

    upload02

--- include/db.php
+++ include/db.php
@@ -1,8 +1,7 @@
 <?php 
     $host = 'localhost';
-    // $db_host = "http://제가받은 호스팅주소.cafe24.com/WebMysql/";
-    $user = 'viv0803';
-    $pw = 'ykayou98!';
-    $dbName = 'viv0803';
+    $user = '';
+    $pw = '';
+    $dbName = '';
     $mysqli = new mysqli($host, $user, $pw, $dbName);
 ?>
